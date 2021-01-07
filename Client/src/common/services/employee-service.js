4bd970d7cc399620021070ab6d6f5bdae601fa47
Tuan.Nguyen <tuan.ng.developer@gmail.com>
2021-01-07 16:18:23 +0700

    Remove redundant files

--- Client/src/common/services/employee-service.js
+++
@@ -1,8 +0,0 @@
-import serviceBase from './service-base';
-
-export class EmployeeService extends serviceBase {
-    getEmployees = () => {
-        return this.client.get("/mock/employees.json");
-    }
-}
-
